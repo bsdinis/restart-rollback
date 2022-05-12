288eccd1780c80bf8e840835a8d65e74ef82eae8
bsdinis <baltasar.dinis@tecnico.ulisboa.pt>
2022-05-13 00:19:33 +0100

    [teems]: add scripts to launch redis

---
+++ sbin/launch_redis.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo "you should launch redis as root, using the \`redis-server --bind \$(hostname -I) --protected-mode no\` command"
---
+++ sbin/launch_redis_root.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+redis-server --bind $1 --protected-mode no
